0f474d55d3f0ce8d8e4a4e2a147a1ab57b1c617b
Vedant <vedanthalde1809@gmail.com>
2025-08-03 09:42:26 +0000

    Complete Rock Paper Scissors project

--- RPS.py
+++ RPS.py
@@ -1,10 +1,38 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    import random
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Store opponent moves
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    return guess
+    # Return 'R' for the first few rounds to collect data
+    if len(opponent_history) < 5:
+        return "R"
+
+    # Detect repeating patterns (like Quincy)
+    seq_len = 5
+    if len(opponent_history) > seq_len:
+        recent = opponent_history[-seq_len:]
+        for i in range(len(opponent_history) - seq_len):
+            if opponent_history[i:i+seq_len] == recent:
+                next_index = (i + seq_len) % len(opponent_history)
+                if next_index < len(opponent_history):
+                    predicted = opponent_history[next_index]
+                    return counter_move(predicted)
+
+    # Detect Abbey (she plays the move that beats our last move)
+    if len(opponent_history) >= 2:
+        if opponent_history[-1] == counter_move(opponent_history[-2]):
+            # If opponent keeps playing the counter to our last move, flip the logic
+            predicted = counter_move(opponent_history[-1])
+            return counter_move(predicted)
+
+    # Default to frequency analysis (for Mrugesh)
+    freq = {"R": 0, "P": 0, "S": 0}
+    for move in opponent_history:
+        freq[move] += 1
+    predicted = max(freq, key=freq.get)
+    return counter_move(predicted)
+
+def counter_move(move):
+    return {"R": "P", "P": "S", "S": "R"}[move]
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# This entrypoint file to be used in development. Start by reading README.md
-from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
+from RPS_game import play, quincy, abbey, kris, mrugesh
 from RPS import player
-from unittest import main
 
+print("Testing against all 4 bots:\n")
+
+print("Quincy:")
 play(player, quincy, 1000)
+
+print("\nAbbey:")
 play(player, abbey, 1000)
+
+print("\nKris:")
 play(player, kris, 1000)
+
+print("\nMrugesh:")
 play(player, mrugesh, 1000)
-
-# Uncomment line below to play interactively against a bot:
-# play(human, abbey, 20, verbose=True)
-
-# Uncomment line below to play against a bot that plays randomly:
-# play(human, random_player, 1000)
-
-
-
-# Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
